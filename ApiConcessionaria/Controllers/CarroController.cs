386d97c7abeca32cbdfcb387c74b412282b16767
luizf <luizf@LUIZFERNANDO>
2022-06-29 21:50:13 -0300

    Adicionar arquivos de projeto.

---
+++ ApiConcessionaria/Controllers/CarroController.cs
@@ -0,0 +1,57 @@
+﻿using ApiConcessionaria.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiConcessionaria.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarroController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public CarroController(ApiContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<CarroController>
+        [HttpGet]
+        public IEnumerable<Carro> Get()
+        {
+            return _context.Carro;
+        }
+
+        // GET api/<CarroController>/5
+        [HttpGet("{id}")]
+        public Carro Get(int id)
+        {
+            return _context.Carro.Where(x=>x.Id == id).FirstOrDefault();
+        }
+
+        // POST api/<CarroController>
+        [HttpPost]
+        public void Post(Carro _carro)
+        {
+            _context.Carro.Add(_carro);
+            _context.SaveChanges();
+        }
+
+        // PUT api/<CarroController>/5
+        [HttpPut("{id}")]
+        public void Put(Carro _carro,int id)
+        {
+            _carro.Id = id;
+            _context.Carro.Update(_carro);
+            _context.SaveChanges();
+        }
+
+        // DELETE api/<CarroController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _context.Carro.Remove(_context.Carro.Where(x => x.Id == id).FirstOrDefault());
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ ApiConcessionaria/Controllers/ClienteController.cs
@@ -0,0 +1,70 @@
+﻿using ApiConcessionaria.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiConcessionaria.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public ClienteController(ApiContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<ClienteController>
+        [HttpGet]
+        public IEnumerable<Cliente> Get()
+        {
+            var qry = from  cliente in _context.Cliente 
+                       join carro in _context.Carro   on cliente.IdCarro equals carro.Id
+                      select new Cliente
+                      {
+                          Id = cliente.Id,
+                          Cpf = cliente.Cpf,
+                          Nome = cliente.Nome,
+                          Email = cliente.Email,
+                          Telefone = cliente.Telefone,
+                          Cnh = cliente.Cnh,   
+                          IdCarro = cliente.IdCarro,
+                          Carro = carro
+                      };
+            return qry;
+        }
+
+        // GET api/<ClienteController>/5
+        [HttpGet("{id}")]
+        public Cliente Get(int id)
+        {
+            return _context.Cliente.Where(x=>x.Id == id).FirstOrDefault();
+        }
+
+        // POST api/<ClienteController>
+        [HttpPost]
+        public void Post(Cliente _cliente)
+        {
+            _context.Cliente.Add(_cliente);
+            _context.SaveChanges();
+        }
+
+        // PUT api/<ClienteController>/5
+        [HttpPut("{id}")]
+        public void Put(Cliente _cliente, int id)
+        {
+            _cliente.Id = id;
+            _context.Cliente.Update(_cliente);
+            _context.SaveChanges();
+        }
+
+        // DELETE api/<ClienteController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _context.Cliente.Remove(_context.Cliente.Where(x => x.Id == id).FirstOrDefault());
+            _context.SaveChanges();
+        }
+    }
+}
